38131cfabe0f1ce0b9fab20516df99a79807a302
Jackie Li <jackie.li@fuseim.com>
2018-04-20 15:20:54 +0100

    add main

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 # crawler
 
 A simple web crawler limited to one domain. E.g. when you start with https://monzo.com/, it would crawl all pages within monzo.com, but not follow external links, for example to the Facebook and Twitter accounts. Given a URL, it should print a simple site map, showing the links between pages.
+
+## install
+
+```bash
+go get github.com/jackielii/crawler/cmd/crawler
+```
+
+## run
+
+```bash
+crawler https://monzo.com > monzo.txt
+```
+
+You can use `-v` to turn on a bit logging
--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
 
+	"github.com/jackielii/crawler"
+)
+
+const usage = `Usage:
+	crawler [-v] <url>
+`
+
+func main() {
+	flag.BoolVar(&crawler.Verbose, "v", false, "verbose logging")
+	flag.Parse()
+	if flag.NArg() < 1 {
+		fmt.Println(usage)
+		os.Exit(1)
+	}
+
+	u := flag.Arg(0)
+	page, err := crawler.Crawl(u)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to crawl %s: %v", u, err)
+		os.Exit(2)
+	}
+
+	print(page, 0)
+}
+
+var printed = make(map[string]bool)
+
+func print(p *crawler.Page, indent int) {
+	fmt.Print(strings.Repeat(" ", indent))
+	fmt.Printf("%s \"%s\"\n", p.Info.URI, p.Info.Description)
+	if printed[p.Info.URI] {
+		return
+	}
+	printed[p.Info.URI] = true
+
+	// skip dup on the same level
+	unique := make(map[string]bool)
+	for _, p := range p.Links {
+		if unique[p.Info.URI] {
+			continue
+		}
+		unique[p.Info.URI] = true
+		print(p, indent+2)
+	}
 }
